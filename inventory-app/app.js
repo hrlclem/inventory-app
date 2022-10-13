c401be4362616924e83fd220c11e5702514cf96e
Clement <perso@clementhurel.fr>
2022-10-13 17:00:42 +0900

    database set up and population

--- inventory-app/app.js
+++ inventory-app/app.js
@@ -38,4 +38,20 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+
+
+// MongoDB setup
+// Import the mongoose module
+const mongoose = require("mongoose");
+
+// Set up default mongoose connection
+const mongoDB = "mongodb+srv://<inventoryUsername>:<inventoryPassword>@cluster0.xgpkagb.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Get the default connection
+const db = mongoose.connection;
+
+// Bind connection to error event (to get notification of connection errors)
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
 module.exports = app;
