3acc6b6c042f239d707786954f2793beaa0173af
Clement <perso@clementhurel.fr>
2022-10-13 21:43:58 +0900

    final routes dummy setup

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const catalogRouter = require("./routes/catalog");
 
-var app = express();
+
+const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -20,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use("/catalog", catalogRouter);
 
 app.use(function(req, res, next) {
   next(createError(404));
@@ -39,7 +42,7 @@ app.use(function(err, req, res, next) {
 // MongoDB setup
 const mongoose = require("mongoose");
 
-const mongoDB = "mongodb+srv://<inventoryUsername>:<inventoryPassword>@cluster0.xgpkagb.mongodb.net/?retryWrites=true&w=majority";
+const mongoDB = "mongodb+srv://inventoryUsername:inventoryPassword@cluster0.xgpkagb.mongodb.net/?retryWrites=true&w=majority";
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
