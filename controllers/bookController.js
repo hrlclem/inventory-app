8ef46caf780a63eb9b4174c180ac35f749babefc
Clement <perso@clementhurel.fr>
2022-10-15 10:06:22 +0900

    linking routes and views

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,10 +1,37 @@
 const { body, validationResult } = require("express-validator");
 const async = require('async');
-const BookInstance = require('../models/bookinstance');
 const Book = require("../models/book");
+const Author = require("../models/author");
+const Genre = require("../models/genre");
+const BookInstance = require("../models/bookinstance");
 
-exports.index = (req, res) => {
-  res.send("NOT IMPLEMENTED: Site Home Page");
+exports.index = function (req, res) {
+  async.parallel(
+    {
+      book_count: function (callback) {
+        Book.countDocuments({}, callback);
+      },
+      book_instance_count: function (callback) {
+        BookInstance.countDocuments({}, callback);
+      },
+      book_instance_available_count: function (callback) {
+        BookInstance.countDocuments({ status: "Available" }, callback);
+      },
+      author_count: function (callback) {
+        Author.countDocuments({}, callback);
+      },
+      genre_count: function (callback) {
+        Genre.countDocuments({}, callback);
+      },
+    },
+    function (err, results) {
+      res.render("index", {
+        title: "Local Library Home",
+        error: err,
+        data: results,
+      });
+    }
+  );
 };
 
 exports.book_list = (req, res) => {
